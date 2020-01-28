82f5db7846427375817151a31f20cec25961613f
Neri <neri.garciafigueroa@gmail.com>
2020-01-27 16:29:42 -0800

    Committing second part of the project

---
+++ flixer/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixer
+//
+//  Created by Neri Garcia on 1/27/20.
+//  Copyright © 2020 Neri Garcia. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
